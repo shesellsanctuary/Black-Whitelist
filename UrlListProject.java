47e2a0afa55033db284e7074881ed818f820cd26
Emily Calvet <emily.calvet@sap.com>
2018-12-12 17:24:17 -0200

    hello world in java

---
+++ UrlListProject.java
@@ -0,0 +1,14 @@
+class UrlListProject { 
+   public static void main(String[] args) { 
+      if (args.length == 1){
+         String arg = args[0];
+         String outString = "Hello World! and ";
+         System.out.println(outString + arg); 
+      }else{
+         System.out.println("Hello World!"); 
+      }
+   }
+}
+
+
+
