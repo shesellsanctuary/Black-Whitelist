e58a6972439f2ae3d48f3152dad9c53e81fc1d77
Emily Calvet <emily.calvet@sap.com>
2018-12-12 23:26:55 -0200

    created interpreter and validete url

--- UrlListProject.java
+++ UrlListProject.java
@@ -1,14 +1,76 @@
+import java.io.*; 
 class UrlListProject { 
    public static void main(String[] args) { 
-      if (args.length == 1){
-         String arg = args[0];
-         String outString = "Hello World! and ";
-         System.out.println(outString + arg); 
-      }else{
-         System.out.println("Hello World!"); 
-      }
+     Interpreter interpreter = new Interpreter();
+     interpreter.interpret(args);
    }
 }
 
+class Interpreter {
+   static void interpret (String[] args) {
+      String command;
 
+      if (args.length == 2){
+         command = args[0];
+         String url = args[1];
+         if (!isValidUrl(url)) {
+            System.out.println("Invalid url, please provide a valid url.");
+         } else {
+            switch(command){
+               case "verify":    Checker ck = new Checker();
+                                 ck.verify(url);
+                                 break; // verify where url is and print it
+               case "add-whitelist": 
+               case "add-blacklist":
+               case "remove-whitelist":
+               case "remove-blacklist":
+               default: System.out.println("Invalid command, please provide one of the available commands for url's");
+            }
+         }
+      }else if(args.length == 1){
+         command = args[0];
+
+         System.out.println(command); 
+      }else {
+         System.out.println("No arguments or too many provided, please provide any of the available commands with the desired url or the show commands."); 
+      }
+   }
+
+   static boolean isValidUrl (String url) {
+      String urlRegex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+
+      return (url.matches(urlRegex));
+   }
+}
+class Checker {
+   static void verify (String url) {
+      char output;
+      
+      System.out.println(url);
+      if (checklist("whitelist", url)) {
+         output = "safe";
+      } else if (checklist("blacklist", url)) {
+         output = "unsafe";
+      } else {
+         output = "Unknown url";
+      }
+
+      System.out.println(output);
+   }
+
+   // static boolean checkList (String listName, String url) {
+   //    if (listName != "whitelist" && listName !="blacklist"){
+   //       System.out.println("Invalid list name");
+   //    }else{
+   //       BufferedReader reader = new BufferedReader(new FileReader("./"+ listName + ".txt"));
+   //       String line;
+   //       while ((line = br.readLine()) != null) {
+   //          if (line == url){
+   //             return true;
+   //          }
+   //       }
+   //       return false;
+   //    }
+   // } 
+}
 
