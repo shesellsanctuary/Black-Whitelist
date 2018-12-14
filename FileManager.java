a4e92a08763f0013da00b6eac3fc0cccf772951c
Emily Calvet <emily.calvet@sap.com>
2018-12-14 15:14:23 -0200

    add to list and show list working

--- FileManager.java
+++ FileManager.java
@@ -1,5 +1,7 @@
 import java.io.*;
 
+import jdk.nashorn.internal.runtime.regexp.RegExpResult;
+
 class FileManager {
 
     public void checkFiles (String... files) throws IOException{
@@ -10,23 +12,86 @@ class FileManager {
     }
 
     public void printList (String listName) {
-        
+        try{
+            File file = checkFile(listName);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+               System.out.println(line);
+            }
+            reader.close();
+        }catch(IOException e){
+            System.out.println(e);
+        }
     }
 
     public void addToList (String listName, String url) {
-
+        if (isInFile(listName, url) == false){
+            try{
+                FileWriter file = new FileWriter(listName + ".txt", true);
+                BufferedWriter writer = new BufferedWriter(file);
+                writer.append(url);
+                writer.newLine();
+                writer.close();
+                System.out.println("Url added to " + listName + ".");
+            }catch(IOException e){
+                System.out.println(e);
+            }
+        }
     }
 
     public void removeFromList (String listName, String url) {
-        
+        try{
+            File file = checkFile(listName);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+               if (line == url) {
+                    line.replace(url, "");
+                    reader.close();
+                    System.out.println("Url removed from " + listName + ".");
+               }
+            }
+            System.out.println("Url not in list to be removed.");
+            reader.close();
+
+        }catch(IOException e){
+            System.out.println(e);
+        }
     }
 
-    private void checkFile (String fileName) throws IOException{
+    private File checkFile (String fileName) throws IOException{
         File file = new File("./", fileName + ".txt");
         if(!file.exists()){
             if(!file.createNewFile()){
                 throw new IOException("Error creating new file: " + file.getAbsolutePath());
+            }else{
+                return file;
             }
-        } 
+        }else {
+            return file;
+        }
+    }
+
+    private boolean isInFile (String fileName, String url) {
+        try{
+            File file = checkFile(fileName);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.printf("line= ",line);
+                if (line == url) {
+                    System.out.println("Url is already on " + fileName + ".");
+                    reader.close();
+                    return true;
+                }
+            }
+            reader.close();
+            return false;
+        }catch(IOException e){
+            System.out.println(e);
+            return true;
+        }
+        
     }
 }
--- Interpreter.java
+++ Interpreter.java
@@ -1,36 +1,55 @@
 import java.io.*;
 
 class Interpreter {
-    public void interpret (String[] args) {
-       String command;
-       if (args.length == 2){
-          command = args[0];
-          String url = args[1];
-          if (!isValidUrl(url)) {
-             System.out.println("Invalid url, please provide a valid url.");
-          } else {
-             switch(command){
-                case "verify":    Checker ck = new Checker();
-                                  ck.verify(url);
-                                  break; // verify where url is and print it
-                case "add-whitelist": 
-                case "add-blacklist":
-                case "remove-whitelist":
-                case "remove-blacklist":
-                default: System.out.println("Invalid command, please provide one of the available commands for url's");
-             }
-          }
-       }else if(args.length == 1){
-          command = args[0];
- 
-          System.out.println(command); 
-       }else {
-          System.out.println("No arguments or too many provided, please provide any of the available commands with the desired url or the show commands."); 
-       }
-    }
- 
-    boolean isValidUrl (String url) {
-       String urlRegex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-       return (url.matches(urlRegex));
-    }
- }
+   public void interpret(String[] args) {
+      String command = args[0];
+      if (args.length == 2) {
+         String url = args[1];
+         if (!isValidUrl(url)) {
+            System.out.println("Invalid url, please provide a valid url.");
+         } else {
+            FileManager fileManager = new FileManager();
+            switch (command) {
+            case "verify":
+               Checker ck = new Checker();
+               ck.verify(url);
+               break;
+            case "add-whitelist":
+               fileManager.addToList("whitelist", url);
+               break;
+            case "add-blacklist":
+               fileManager.addToList("blacklist", url);
+               break;
+            case "remove-whitelist":
+               fileManager.removeFromList("whitelist", url);
+               break;
+            case "remove-blacklist":
+               fileManager.removeFromList("blacklist", url);
+               break;
+            default:
+               System.out.println("Invalid command, please provide one of the available commands for url's");
+            }
+         }
+      } else if (args.length == 1) {
+         FileManager fileManager = new FileManager();
+            switch (command) {
+            case "show-whitelist":
+               fileManager.printList("whitelist");
+               break;
+            case "show-blacklist":
+               fileManager.printList("blacklist");
+               break;
+            default:
+            System.out.println("Invalid command.");
+            }
+      } else {
+         System.out.println(
+               "No arguments or too many provided, please provide any of the available commands with the desired url or the show commands.");
+      }
+   }
+
+   private boolean isValidUrl(String url) {
+      String urlRegex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+      return (url.matches(urlRegex));
+   }
+}
